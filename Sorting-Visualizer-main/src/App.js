641175c20bb882b3f34f5fc92bc457f536a55aa7
yadvend <yadvendrasingh586@gmail.com>
2022-08-19 15:41:33 +0530

    first commit

---
+++ Sorting-Visualizer-main/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+import SortingVisualiser from "./Sorting Visualiser/SortingVisualiser";
+
+function App() {
+  return (
+    <div className="App">
+      <SortingVisualiser />
+    </div>
+  );
+}
+
+export default App;
